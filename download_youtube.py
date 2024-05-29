e9598b5e302b3286d83cd2d14205c48e3d08ba2c
Abraham E. Hanson <ekenehanson@gmail.com>
2024-05-29 14:36:43 +0100

    first commit

---
+++ download_youtube.py
@@ -0,0 +1,15 @@
+import yt_dlp
+
+def main():
+
+    url = input("Please enter the video URl: ")
+
+    ydl_opts = {}
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    print("Video downloaded Successfully!!!")
+
+if __name__ == "__main__":
+    main()
